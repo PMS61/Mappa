cf354e16945c74cb2b6486976d89f5a1cf3ffacd
harshal-ishere <harshal011kamble@gmail.com>
2025-03-08 23:56:14 +0530

    niceeeee

--- client/actions/allRepos.jsx
+++ client/actions/allRepos.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 
 export default async function fetchReposAction() {
   try {
-    const token = cookies().get("token")?.value;
+    // const token = cookies().get("token")?.value;
     const org_id = cookies().get("org_id")?.value;
     const res = await axios.post(`http://localhost:8000/repo/get-repos/`, {
-      uid: token,
+      // uid: token,
       org_id: org_id,
     });
     if (res.status === 200 && !res.data.error) {
--- server/routes/allRepos.py
+++ server/routes/allRepos.py
@@ -7,7 +7,6 @@ from pydantic import BaseModel
 from uuid import UUID
 
 class AReqMod(BaseModel):
-    uid: str
     org_id: UUID
 
 router = APIRouter()
@@ -19,7 +18,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 @router.post("/get-repos/", response_model=List[ARepoResMod])
 async def get_repos_by_uid_and_org(req: AReqMod):
     print(req)
-    response = supabase.table("repo").select("*").eq("uid", req.uid).eq("org_id", str(req.org_id)).execute()
+    response = supabase.table("repo").select("*").eq("org_id", str(req.org_id)).execute()
 
     if not response.data:  # Check if no repositories found
         raise HTTPException(status_code=404, detail="No repositories found for this UID and Org ID")
